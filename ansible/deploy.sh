5aefd0bd418aac99044d8a24c3a3d2033ff01491
David García González <david.garciaGD8@comunidadunir.net>
2021-07-18 00:21:48 -0500

    Commit Ansible
    Configuración de 3 Nodos
    
    Instalacion de Kubernetes
    Instalación y Configuración de NFS
    Despliegue de WebApp con Volume

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,1 +1,6 @@
-
+ansible-playbook -i hosts 00-playbook-all-nodes.yml
+ansible-playbook -i hosts 01-playbook-nfs.yml
+ansible-playbook -i hosts 02-playbook-workers_master.yml
+ansible-playbook -i hosts 03-playbook-master.yml
+ansible-playbook -i hosts 04-playbook-workers.yml
+ansible-playbook -i hosts 05-deploy-app.yml
